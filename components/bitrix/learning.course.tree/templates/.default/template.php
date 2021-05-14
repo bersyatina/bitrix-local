45cf5f51be75cd76321c0681cf97b5016e8a8090
bersyatina <aw_apanasenko@mail.ru>
2021-05-14 15:28:58 +0500

    14,05,2021

--- components/bitrix/learning.course.tree/templates/.default/template.php
+++ components/bitrix/learning.course.tree/templates/.default/template.php
@@ -1,16 +1,13 @@
-<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-use Bitrix\Main\UI\Extension;
-Extension::load('ui.bootstrap4');
-?>
+<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 
 <?if (!empty($arResult["ITEMS"])):?>
 
 <div>
-    <ul сlass="list-group">
+    <div class="list-group">
 	<?
 		$bracketLevel = 0;
 		foreach ($arResult["ITEMS"] as $arItem):
-			if ( $arItem["DEPTH_LEVEL"] <= $bracketLevel )
+    		if ( $arItem["DEPTH_LEVEL"] <= $bracketLevel )
 			{
 				$deltaLevel = $bracketLevel - $arItem['DEPTH_LEVEL'] + 1;
 				echo str_repeat("</ul></li>", $deltaLevel);
@@ -21,6 +18,7 @@ Extension::load('ui.bootstrap4');
 		<?if ($arItem["TYPE"] == "CH"):
 			$bracketLevel++;
 		?>
+
 			<li<?if($arItem["CHAPTER_OPEN"] === false):?> class="close"<?elseif($arItem["SELECTED"] === true):?> class="selected"<?endif?>>
 				<div class="chapter" <?php if (!$arItem['DELAYED']) { ?>onClick="JMenu.OpenChapter(this,'<?=$arItem["ID"]?>')"<?php } ?>></div>
 				<div class="item-text"><a
@@ -42,17 +40,16 @@ Extension::load('ui.bootstrap4');
 							)
 							. ')';
 					}
-					?></a>Тест4</div>
-				<ul>
+					?></a></div>
+				<div>
 		<?elseif($arItem["TYPE"] == "LE"):?>
-			<li>
-				<div class="item-text"><a <?php if ($arItem['DELAYED']) echo 'style="color:gray;"'; ?>
+            <a <?php if ($arItem['DELAYED']) echo 'style="color:gray;"'; ?>
 					href="<?php
 					if ( ! $arItem['DELAYED'] )
 						echo $arItem["URL"];
 					else
 						echo 'javascript:void(0);';
-				?>"<?if($arItem["SELECTED"]):?> class="list-group-item list-group-item-action list-group-item-secondary"<?else:?> class="list-group-item list-group-item-action list-group-item-secondary" <?endif?>><?php
+				?>"<?if($arItem["SELECTED"]):?> class="list-group-item list-group-item-action list-group-item-primary active"<?else:?> class="list-group-item list-group-item-action list-group-item-primary" <?endif?>><?php
 					echo $arItem["NAME"];
 					if ($arItem['DELAYED'])
 					{
@@ -64,25 +61,20 @@ Extension::load('ui.bootstrap4');
 							)
 							. ')';
 					}
-					?></a></div>
-			</li>
+					?></a>
 		<?elseif($arItem["TYPE"] == "CD"):?>
-			<li>
-				<div class="item-text list-group"><a href="<?=$arItem["URL"]?>"<?if($arItem["SELECTED"]):?> class="list-group-item list-group-item-action list-group-item-secondary"<?else:?> class="list-group-item list-group-item-action list-group-item-secondary"<?endif?>><?=$arItem["NAME"]?></a></div>
-			</li>
+			<a href="<?=$arItem["URL"]?>"<?if($arItem["SELECTED"]):?> class="list-group-item list-group-item-action list-group-item-primary active"<?else:?> class="list-group-item list-group-item-action list-group-item-primary"<?endif?>><?=$arItem["NAME"]?></a>
 		<?elseif($arItem["TYPE"] == "TL"):?>
-			<li>
-				<div class="item-text"><a href="<?=$arItem["URL"]?>"<?if($arItem["SELECTED"]):?> class="list-group-item list-group-item-action list-group-item-secondary"<?else:?> class="list-group-item list-group-item-action list-group-item-secondary"<?endif?>><?=$arItem["NAME"]?></a></div>
-			</li>
+			<a href="<?=$arItem["URL"]?>"<?if($arItem["SELECTED"]):?> class="list-group-item list-group-item-action list-group-item-primary active"<?else:?> class="list-group-item list-group-item-action list-group-item-primary"<?endif?>><?=$arItem["NAME"]?></a>
 		<?endif?>
 
 	<?endforeach?>
 
-	</ul>
+	</div>
 </div>
 
 <script type="text/javascript">
-	var JMenu = new JCMenu('<?=(array_key_exists("LEARN_MENU_".$arParams["COURSE_ID"],$_COOKIE ) ? CUtil::JSEscape($_COOKIE["LEARN_MENU_".$arParams["COURSE_ID"]]) :"")?>', '<?=$arParams["COURSE_ID"]?>');
+    var JMenu = new JCMenu('<?=(array_key_exists("LEARN_MENU_".$arParams["COURSE_ID"],$_COOKIE ) ? CUtil::JSEscape($_COOKIE["LEARN_MENU_".$arParams["COURSE_ID"]]) :"")?>', '<?=$arParams["COURSE_ID"]?>');
 </script>
 
 <?endif?>
